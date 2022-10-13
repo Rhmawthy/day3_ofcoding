b4428fbb83e76436e8ef4754e6ff44e7bff37cda
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-10-13 17:29:06 +0800

    Add files via upload

---
+++ day3_type_data.py
@@ -0,0 +1,22 @@
+#type data pada python
+#type data merupakan macam-macam data yang bisa kita pakai dan bisa kita msukkan ke dalam variabel
+#type data pada phyton yaitu:
+
+#type data integer (angka bilangan bulat)
+a = 7
+print(type(a))
+
+#type data fload, angka yang menggunakan koma (desimal)
+b = 2.5
+print(type(b))
+
+#type data boolean merupakan type data yang hanya bernilai true dan false
+c = True
+print(type(c))
+
+#type data string merupakan tipe data yang terdiri dari huruf atau kalimat yang memiliiki ciri menggunakan tanda kutip dua ("")
+d = "semangat belajar"
+print(type(d))
+    
+
+
